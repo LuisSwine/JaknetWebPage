aa97d40431363fdf49e749bbd703b4c205ab1dff
EduardoLeonCastillo <eduardoleon1266@gmail.com>
2023-06-29 17:20:55 -0600

    todas las rutas listas

--- routes/router.js
+++ routes/router.js
@@ -5,5 +5,29 @@ router.get('/home', (req,res)=>{
     res.render('index');
 })
 
+router.get('/blog', (req, res) => {
+    res.render('blog'); 
+});
+
+router.get('/servicios', (req, res) => {
+    res.render('servicios'); 
+});
+
+router.get('/contacto', (req, res) => {
+    res.render('contacto'); 
+});
+
+router.get('/perfil', (req, res) => {
+    res.render('perfil'); 
+});
+
+router.get('/login', (req, res) => {
+    res.render('login'); 
+});
+
+router.get('/registro', (req, res) => {
+    res.render('registro'); 
+});
+
 
 module.exports = router
