dac7efe9ce0ae8e372e386105dd9a9df82d14bc8
EduardoLeonCastillo <eduardoleon1266@gmail.com>
2023-07-07 13:32:47 -0600

    actualizacion completa

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const nodemailer = require('nodemailer')
 const router = express.Router()
 
 router.get('/home', (req,res)=>{
@@ -27,6 +28,49 @@ router.get('/login', (req, res) => {
 
 router.get('/registro', (req, res) => {
     res.render('registro'); 
+});
+
+router.post('/send-email', (req, res) =>{
+    console.log(req.body); 
+    const { Nombre, email, Telefono, Mensaje} = req.body;
+    contentHTML = `
+        <h1>Informacion Usuario</h1>
+        <ul>
+            <li>Nombre: ${Nombre}</li>
+            <li>Correo: ${email}</li>
+            <li>Telefono: ${Telefono}</li>
+        <ul>
+        <p>${Mensaje}</p>
+    
+    `;
+    // Configurar el transportador de correo electrónico
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: '',
+            pass: 'ddqlccckcfkcilzm'
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: 'eduardoleon1266@gmail.com',
+        subject: 'NodeJs',
+        html: contentHTML
+      };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+          res.send('Error al enviar el correo electrónico');
+        } else {
+          console.log('Correo electrónico enviado: ' + info.response);
+          res.send('Correo electrónico enviado correctamente');
+        }
+      });
+    res.redirect('/contacto')
 });
 
 
