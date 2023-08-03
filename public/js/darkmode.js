bc094e3b74b988709d08419c39c834a29187684b
EduardoLeonCastillo <eduardoleon1266@gmail.com>
2023-08-03 15:49:00 -0600

    Modo oscuro actualizado

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -5,14 +5,27 @@ function toggleDarkMode() {
   
     // Guardar la preferencia en el almacenamiento local
     localStorage.setItem('darkMode', body.classList.contains('dark-mode'));
-  
-    // Actualizar el texto del botón según el modo actual
-    const darkModeButton = document.getElementById('darkModeButton');
-    darkModeButton.textContent = isDarkMode ? 'Modo Claro' : 'Modo Oscuro';
   }
   
-    // Verificar y aplicar la preferencia del modo oscuro al cargar la página
-if (localStorage.getItem('darkMode') === 'true') {
-    document.querySelector('body').classList.add('dark-mode');
-    document.getElementById('darkModeToggle').checked = true;
-  }
+//     // Verificar y aplicar la preferencia del modo oscuro al cargar la página
+// if (localStorage.getItem('darkMode') === 'true') {
+//     document.querySelector('body').classList.add('dark-mode');
+//     document.getElementById('darkModeToggle').checked = true;
+//   }
+
+// Verificar y aplicar la preferencia del modo oscuro al cargar la página
+function applyDarkModePreference() {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const isUserPreferredDarkMode = localStorage.getItem('darkMode') === 'true';
+
+  const body = document.querySelector('body');
+  body.classList.toggle('dark-mode', prefersDarkMode || isUserPreferredDarkMode);
+  document.getElementById('darkModeToggle').checked = prefersDarkMode || isUserPreferredDarkMode;
+}
+
+// Aplicar la preferencia del modo oscuro al cargar la página
+applyDarkModePreference();
+
+// Agregar listener al botón de switch para alternar el modo oscuro
+const darkModeToggle = document.getElementById('darkModeToggle');
+darkModeToggle.addEventListener('click', toggleDarkMode);
