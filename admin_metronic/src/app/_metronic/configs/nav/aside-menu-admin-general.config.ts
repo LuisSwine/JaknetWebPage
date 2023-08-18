9a6c0e17c5aa88b1fd551b3a1c06c5bf216215a2
EduardoLeonCastillo <eduardoleon1266@gmail.com>
2023-08-17 18:09:42 -0600

    control de categorias

--- admin_metronic/src/app/_metronic/configs/nav/aside-menu-admin-general.config.ts
+++ admin_metronic/src/app/_metronic/configs/nav/aside-menu-admin-general.config.ts
@@ -26,5 +26,21 @@ export const AsideMenuAdminGeneral = {
           }
         ]
       },
+      { section: 'Productos' },
+      {
+        title: 'Categorias',
+        root: true,
+        name: "categorias",
+        bullet: 'dot',
+        icon: 'flaticon2-user-outline-symbol',
+        svg: './assets/media/svg/icons/Devices/CPU2.svg',
+        page: '/categorias',
+        submenu: [
+          {
+            title: 'Lista Categorias',
+            page: '/categorias/list'
+          }
+        ]
+      },
     ]
 }
